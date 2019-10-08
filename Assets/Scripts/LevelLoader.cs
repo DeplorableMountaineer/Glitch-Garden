295cb0e104600e9e2b307b2a7089da47c86a8e71
tdvance <deplorable.mountaineer@gmail.com>
2019-10-08 11:45:47 -0400

    Set up splash screen and start screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+    [SerializeField] private float waitTime = 3;
+    [SerializeField] private string levelToLoad = "Start Screen";
+
+    // Start is called before the first frame update
+    IEnumerator Start() {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
