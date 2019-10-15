c113169658dd8236dc2f37116df8c4a63201b3a8
tdvance <deplorable.mountaineer@gmail.com>
2019-10-15 15:59:10 -0400

    Spawn lizard attackers and have them start moving when walking animation starts

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+    [SerializeField] private bool spawn = true;
+    [SerializeField] private float minSpawnDelay = 1;
+    [SerializeField] private float maxSpawnDelay = 5;
+    [SerializeField] private Attacker attackerPrefab;
+
+    IEnumerator Start() {
+        while(spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker() {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    void Update() {
+    }
+}
