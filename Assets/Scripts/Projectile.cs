3fc0427f18d398616513f13a4d1840ed353af386
tdvance <deplorable.mountaineer@gmail.com>
2019-10-17 13:46:41 -0400

    Cactus fires zucchinis

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+    [SerializeField] private Vector2 velocity = Vector2.right;
+    [SerializeField] private float rotationSpeed = -Mathf.PI/2;
+
+    void Start() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(velocity, ForceMode2D.Impulse);
+        rb.AddTorque(rotationSpeed, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Vector3 offset;
+
+    public void Fire() {
+        Instantiate(projectilePrefab, transform.position + offset, Quaternion.identity);
+    }
+}
