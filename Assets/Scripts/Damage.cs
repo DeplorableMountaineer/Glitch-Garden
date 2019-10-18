e564bc44ee8c4b258936a18b421ae00b727c784c
tdvance <deplorable.mountaineer@gmail.com>
2019-10-18 12:09:54 -0400

    Cactus kills the Lizard  in the Garden with a Zucchini

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+    [SerializeField] private float amount = 50;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger");
+        Health h = other.gameObject.GetComponent<Health>();
+        if(h && other.gameObject.GetComponent<Attacker>()) {
+            h.AddToHealth(amount);
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField] private float health = 100;
+
+    public void AddToHealth(float amount) {
+        health -= amount;
+        if(health <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
+
